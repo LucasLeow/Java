c6ea5293fdf5b3b9b6f95c7a9087a00c263a7978
LucasLeow <leowjianhoong@gmail.com>
2022-12-04 22:49:34 +0800

    Completed Learning Java'

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Math.pow(2,5));
+    Student student1 = new Student("Lucas", "Leow", 2023,4.61, "Aerospace Engineering" );
+    Student student2 = new Student("Jane", "Doe", 2022, 2.8,"Physical Sciences");
 
+    System.out.println(student1.first + " expected Graduation Year: " + student1.performance());
+    System.out.println(student2.first + " expected Graduation Year: " + student2.performance());
     }
 }
