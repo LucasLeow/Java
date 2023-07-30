c68cb720c73ec6ed885b274f8c19019cee440228
LucasLeow <leowjianhoong@gmail.com>
2023-07-30 21:17:48 +0800

    Renamed some dir + completed custom properties

--- SpringBoot/UdemyBiz_SpringBoot3/01-spring-boot-overview/05-cmd-demo/src/main/java/com/lucaslearning/springboot/demo/mycoolapp/rest/MyRestController.java
+++ SpringBoot/UdemyBiz_SpringBoot3/01-spring-boot-overview/05-cmd-demo/src/main/java/com/lucaslearning/springboot/demo/mycoolapp/rest/MyRestController.java
@@ -1,10 +1,16 @@
 package com.lucaslearning.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello, World!";
@@ -13,5 +19,10 @@ public class MyRestController {
     @GetMapping("/about")
     public String aboutMe() {
         return "About me";
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + " | Team Name: " + teamName;
     }
 }
